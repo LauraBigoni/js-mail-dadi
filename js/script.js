41fe7c9c9ebd017abada316ebdd8b72412d38468
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-10 14:03:11 +0100

    added files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Traccia:
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+*/
+
+// 1 - 
+// 2 - 
+// 3 - 
+// 4 - 
+// 5 - 
+// 6 - 
+
---
+++ js/script2.js
@@ -0,0 +1,14 @@
+/*
+Traccia:
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.
+*/
+
+// 1 - 
+// 2 - 
+// 3 - 
+// 4 - 
+// 5 - 
+// 6 - 
+
